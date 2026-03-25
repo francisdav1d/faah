309d5b48f6d857bba5351af0094f45a886a794cc
Yeswanth Ram <your_github_email@example.com>
2026-03-25 23:10:33 +0530

    album strip: added

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,12 +1,14 @@
 import "./App.css"
 import Navbar from "./components/Navbar/navbar"
 import  Hero from "./components/Hero/hero"
+import Cards from "./components/Hero/Cards"
 
 export default function App(){
     return(
         <div className="root">
             <Navbar/>
             <Hero/>
+            <Cards/>
         </div>
     )
 }
